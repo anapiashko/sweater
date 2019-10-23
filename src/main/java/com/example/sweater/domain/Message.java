d0c1a7d610e08e1e8ca53bf26e13d841b9f01531
Anastasiya Piashko <anapiashko@gmail.com>
2019-10-23 19:19:50 +0300

    #picture

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -16,6 +16,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User user;
 
+    private String filename;
+    
     public Message(){
 
     }
@@ -59,5 +61,13 @@ public class Message {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
